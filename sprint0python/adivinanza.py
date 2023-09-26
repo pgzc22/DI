d8474357f7e4dabb6a8a83056b74921dc2bebfa8
Paulo González <pgonzalezc22@fpcoruna.afundacion.org>
2023-09-26 08:50:59 +0200

    Ejercicio 1

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,8 @@
+print("¿De qué color es el caballo blanco de Santiago")
+print("a - Blanco")
+print("b - Negro")
+print("c - Marrón")
+answer = input()
+if answer=="a":
+    print ("Respuesta correcta")
+else: print ("Error")
