f089117b4ee62c86df5b09d3f8c39c00e5ac5500
Paulo González <pgonzalezc22@fpcoruna.afundacion.org>
2023-09-27 10:12:56 +0200

    Ejercicio 6

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,3 +1,4 @@
+from random import sample
 dic = {
 '¿De qué color es el caballo blanco de Santiago?': ['Blanco','Negro','Marrón'],
 'En este banco hay sentado un padre y un hijo, el Padre se llama Juan, el hijo, ya te lo he dicho': ['Elías','Miguel','Esteban'],
@@ -5,16 +6,21 @@ dic = {
 }
 n=-1
 spek=0
-mec = ['a','c','c']
-for i in dic:
-    n=n+1
-    print (i)
-    pa = dic[i]
+sol = {
+'¿De qué color es el caballo blanco de Santiago?': 'a',
+'En este banco hay sentado un padre y un hijo, el Padre se llama Juan, el hijo, ya te lo he dicho': 'c',
+'Oro parece, plata no es': 'c',
+}
+for i in sample(list(dic.items()),2):
+    q=str(i).split("', ")[0]
+    q=q.replace('(\'','')
+    print (q)
+    pa = dic[q]
     print ('a - ' + pa[0])
     print ('b - ' + pa[1])
     print ('c - ' + pa[2])
     pec = input()
-    if pec != mec [n]: 
+    if pec != sol [q]: 
         print('Error')
         spek = spek-5
     else:
