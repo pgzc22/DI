b44bfca36906fe74163f8838ff26cff8e61691a4
Paulo González <pgonzalezc22@fpcoruna.afundacion.org>
2023-10-06 09:40:09 +0200

    Ejercicio 6

--- sprint1Tkinter/catalog/cell.py
+++ sprint1Tkinter/catalog/cell.py
@@ -5,4 +5,4 @@ class Cell:
     def __init__(self, title, path):
         self.title = title
         self.path = path
-        self.image_tk = ImageTk.PhotoImage(file=self.path)
+        self.image_tk = ImageTk.PhotoImage(Image.open(self.path).resize((100,100),resample=Image.LANCZOS))
--- sprint1Tkinter/catalog/window.py
+++ sprint1Tkinter/catalog/window.py
@@ -2,6 +2,7 @@ from tkinter import ttk
 import tkinter as tk
 from cell import Cell
 from tkinter import messagebox
+from PIL import Image, ImageTk
 
 class MainWindow:
     def on_button_clicked(self, cell):
@@ -10,12 +11,13 @@ class MainWindow:
     def __init__(self,root):
         root.title("MainWindow")
         self.cells = [
-            Cell("Emperor Ku","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\edited\\Emperor_Ku.png"),
-            Cell("Emperor Shun","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\edited\\EmperorShun.png"),
-            Cell("Emperor Yao","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\edited\\EmperorYao.png"),
-            Cell("Emperor Zhuanxu","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\edited\\EmperorZhuanxu.png"),
-            Cell("Yellow Emperor","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\edited\\Yellowemperor.png"),
+            Cell("Emperor Ku","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\Emperor_Ku.jpg"),
+            Cell("Emperor Shun","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorShun.jpg"),
+            Cell("Emperor Yao","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorYao.jpg"),
+            Cell("Emperor Zhuanxu","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorZhuanxu.jpg"),
+            Cell("Yellow Emperor","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\Yellowemperor.jpg"),
         ]
+
         for i, cell in enumerate(self.cells):
             label = ttk.Label(root, image=cell.image_tk, text=cell.title, compound=tk.BOTTOM)
             label.grid(row=i, column=0)
