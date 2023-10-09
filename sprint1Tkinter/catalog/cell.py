fd196990f88decf45903af6e3ed7dbc837cc0259
Paulo González <pgonzalezc22@fpcoruna.afundacion.org>
2023-10-09 17:45:07 +0200

    Ejercicio 7

--- sprint1Tkinter/catalog/cell.py
+++ sprint1Tkinter/catalog/cell.py
@@ -2,7 +2,10 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 class Cell:
-    def __init__(self, title, path):
+    def __init__(self, title, description, path):
         self.title = title
+        self.description = description
         self.path = path
+        # lanczos generally produces better results but its more computationally intensive than bicubic,
+        # unless the computer that will run the code is a pos, use lanczos
         self.image_tk = ImageTk.PhotoImage(Image.open(self.path).resize((100,100),resample=Image.LANCZOS))
--- sprint1Tkinter/catalog/window.py
+++ sprint1Tkinter/catalog/window.py
@@ -2,20 +2,19 @@ from tkinter import ttk
 import tkinter as tk
 from cell import Cell
 from tkinter import messagebox
-from PIL import Image, ImageTk
+from detail_window import DetailWindow
 
 class MainWindow:
     def on_button_clicked(self, cell):
-        message = "Has hecho click en la celda"+cell.title
-        messagebox.showinfo("Información", message)
+        DetailWindow(cell)
     def __init__(self,root):
         root.title("MainWindow")
         self.cells = [
-            Cell("Emperor Ku","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\Emperor_Ku.jpg"),
-            Cell("Emperor Shun","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorShun.jpg"),
-            Cell("Emperor Yao","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorYao.jpg"),
-            Cell("Emperor Zhuanxu","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorZhuanxu.jpg"),
-            Cell("Yellow Emperor","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\Yellowemperor.jpg"),
+            Cell("Emperor Ku","Emperor Ku was not the most \nimportant of the Five Emperors, \nso it was not considered as \none of the Three Sovereigns","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\Emperor_Ku.jpg"),
+            Cell("Emperor Shun","Emperor Shun was not the most \nimportant of the Five Emperors, \nso it was not considered as \none of the Three Sovereigns","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorShun.jpg"),
+            Cell("Emperor Yao","Emperor Yao was not the most \nimportant of the Five Emperors, \nso it was not considered as \none of the Three Sovereigns","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorYao.jpg"),
+            Cell("Emperor Zhuanxu","Emperor Zhuanxu was not the most \nimportant of the Five Emperors, \nso it was not considered as \none of the Three Sovereigns","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\EmperorZhuanxu.jpg"),
+            Cell("Yellow Emperor","The Yellow Emperor was the most \nimportant of the Five Emperors, \nto the point it was considered as \none of the Three Sovereigns","C:\\msys64\\home\\Alumno\\DI\\sprint1Tkinter\\catalog\\data\\unedited\\Yellowemperor.jpg"),
         ]
 
         for i, cell in enumerate(self.cells):
